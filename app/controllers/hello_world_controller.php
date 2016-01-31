5a2e5ebfc886793bcc8585f76ca1a9786f927f72
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2016-01-31 17:36:08 +0200

    pusku gittii

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -7,6 +7,18 @@
    	  View::make('suunnitelmat/home.html');
     }
 
+    public static function board(){
+      View::make('suunnitelmat/board.html');
+    }
+
+    public static function newthread(){
+      View::make('suunnitelmat/newthread.html');
+    }
+
+    public static function thread(){
+      View::make('suunnitelmat/thread.html');
+    }
+
     public static function sandbox(){
       // Testaa koodiasi täällä
       View::make('helloworld.html');
--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,20 @@
     HelloWorldController::index();
   });
 
+  $routes->get('/board/1', function() {
+    HelloWorldController::board();
+  });
+
+  $routes->get('/thread/1', function() {
+    HelloWorldController::thread();
+  });
+
+  $routes->get('/board/1/newthread', function() {
+    HelloWorldController::newthread();
+  });
+
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+
