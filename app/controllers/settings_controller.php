85ad2d0cb813ebe05d372b41728175f89ea8ad60
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2016-02-27 21:51:53 +0200

    Päivityksiä

--- app/controllers/settings_controller.php
+++ app/controllers/settings_controller.php
@@ -3,9 +3,16 @@
     class SettingsController extends BaseController {
 
         public static function index() {
-            $settings = ForumSettings::all();
-            
-            View::make("settings/index.html"); 
+            parent::check_logged_in();
+
+            if (parent::has_permission('boardmanagement')) {
+                $settings = ForumSettings::all();
+
+                View::make("settings/index.html", array('settings' => $settings)); 
+            }else {
+                parent::throw_error('Sinulla ei ole oikeuksia hallintapaneeliin!');
+            }
+
         }
 
     }
